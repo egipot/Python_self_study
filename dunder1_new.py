721544d4a8f910d112e9c2807b84641f7c575e74
egipot <92053170+egipot@users.noreply.github.com>
2023-10-20 23:30:11 +0800

    Add files via upload

---
+++ dunder1_new.py
@@ -0,0 +1,18 @@
+#__new__()
+#@python programming FB  @dynamic.coding
+
+#This method helps the constructor __init__() method to create objects for a class.
+#So when we create an instance of a class, the Python interpreter first calls the __new__() method and after that __init__() method
+
+class Sample:
+    def __new__ (self, parameter):
+        print("new invoked", parameter)
+        return super().__new__(self)
+    def __init__(self, parameter):
+        print("init invoked", parameter)
+
+obj = Sample ('a')
+
+# Output:
+# new invoked a 
+# init invoked a
