899decb75200112eb3febbdda010c85c73f269e2
egipot <92053170+egipot@users.noreply.github.com>
2023-10-20 23:38:23 +0800

    Update dunder1_new.py

--- dunder1_new.py
+++ dunder1_new.py
@@ -1,6 +1,8 @@
 #__new__()
 #@python programming FB  @dynamic.coding
 
+#Magic Methods in Python
+
 #This method helps the constructor __init__() method to create objects for a class.
 #So when we create an instance of a class, the Python interpreter first calls the __new__() method and after that __init__() method
 
